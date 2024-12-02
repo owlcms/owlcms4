1a12826b1992b51f01f629a8ee464ad177eb5276
Jean-François Lamy <jf@jflamy.dev>
2024-12-02 11:52:30 -0500

    fix warnings for imports

--- owlcms/src/main/java/app/owlcms/nui/displays/scoreboards/PublicNoLeadersPage.java
+++ owlcms/src/main/java/app/owlcms/nui/displays/scoreboards/PublicNoLeadersPage.java
@@ -1,28 +1,16 @@
 package app.owlcms.nui.displays.scoreboards;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.slf4j.LoggerFactory;
-
-import com.google.common.eventbus.Subscribe;
-import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
 
 import app.owlcms.apputils.queryparameters.DisplayParameters;
 import app.owlcms.apputils.queryparameters.SoundParameters;
 import app.owlcms.data.config.Config;
-import app.owlcms.displays.scoreboard.Results;
-import app.owlcms.displays.scoreboard.ResultsMedals;
 import app.owlcms.i18n.Translator;
 import app.owlcms.init.OwlcmsSession;
-import app.owlcms.uievents.CeremonyType;
-import app.owlcms.uievents.UIEvent;
 import ch.qos.logback.classic.Logger;
 
 @SuppressWarnings("serial")
@@ -32,10 +20,6 @@ public class PublicNoLeadersPage extends PublicScoreboardPage {
 
 	Logger logger;
 	Logger uiEventLogger;
-	Map<String, List<String>> urlParameterMap = new HashMap<>();
-	private Results resultsBoard;
-	protected UI ui;
-	private ResultsMedals medalsBoard;
 
 	public PublicNoLeadersPage() {
 		// intentionally empty. superclass will call init() as required.
